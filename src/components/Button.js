ef316222bb5dcac0627ce9a3eeb27e6b87928ce0
Luana <luanateixeira148@gmail.com>
2021-08-28 22:28:46 -0700

    bring in icons from Font Awesome

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,20 @@
-import React from 'react'; //optional
+import React from 'react';
 import "./Button.scss";
+/* Icons */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
 const classNames = require('classnames');
+
+/* Icons */
+const editIcon = <FontAwesomeIcon icon={faPen} />
+const deleteIcon = <FontAwesomeIcon icon={faTrash} />
+const addIcon = <FontAwesomeIcon icon={faPlus} />
+const saveIcon = <FontAwesomeIcon icon={faCheck} />
+
 
 export default function Button(props) {
   //assign props based on the button class names
@@ -12,14 +26,14 @@ export default function Button(props) {
   });
 
   return (
+
     <button
       className={buttonClass}
       onClick={props.onClick}
       disabled={props.disabled}
     >
-      {props.children}
+      {deleteIcon}
     </button>
   );
 
 }
-
