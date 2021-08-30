a94b43523b6d383f1da20caef231c5ececb032b4
Luana <luanateixeira148@gmail.com>
2021-08-29 17:27:43 -0700

    sort task list by checked and unchecked

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -28,21 +28,60 @@ const tasks = [
     name: "CF Pacific Center",
     address: "701 W Georgia St, Vancouver, BC V7Y 1G5",
     logo_url: "https://images.wirkn.com/w04HAIbNSlGDVcdfI3U4_pa.jpg"
+  },
+  {
+    id: 3,
+    description: "Buy bluelight screen filter",
+    status: false,
+    location_id: 4,
+    device_id: 4,
+    latitude: 49.28334159584531,
+    longitude: -123.11836841961,
+    name: "Dollarama Station Square",
+    address: "Station Square, 6200 McKay Ave, Burnaby, BC V5H 4L7",
+    logo_url: "https://jobapplications.net/wp-content/uploads/dollarama-logo.png"
+  },
+  {
+    id: 4,
+    description: "pickup birthday cake",
+    status: true,
+    location_id: 4,
+    device_id: 4,
+    latitude: 49.28334159584531,
+    longitude: -123.11836841961,
+    name: "Walmart Supercenter",
+    address: "610 6th St, New Westminster, BC V3L 3C2",
+    logo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Walmart_logo.svg/2560px-Walmart_logo.svg.png"
   }
 ];
 
 export default function TaskList(props) {
 
   return (
-    <ul>
-      {tasks.map((task) => (
-        <Show
-          status={task.status}
-          description={task.description}
-          logo_url={task.logo_url}
-        />
-      ))}
-    </ul>
+    <div className="taskList">
+      {/* maps over tasks and returns only unchecked ones */}
+      <ul>
+        {tasks.map(task => task.status === false && (
+          <Show
+            status={task.status}
+            description={task.description}
+            logo_url={task.logo_url}
+          />
+        ))}
+      </ul>
+
+      {/* maps over tasks and returns only checked ones */}
+      <ul>
+        {tasks.map(task => task.status === true && (
+          <Show
+            status={task.status}
+            description={task.description}
+            logo_url={task.logo_url}
+          />
+        ))}
+      </ul>
+    </div>
+
   );
 
 }
