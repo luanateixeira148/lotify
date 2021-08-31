a343e51557370d4015feb5df6a1b63e3d922f6f1
Luana <luanateixeira148@gmail.com>
2021-08-31 12:49:36 -0700

    implement add feature

--- src/components/TaskListItem/Form.js
+++ src/components/TaskListItem/Form.js
@@ -1,21 +1,46 @@
-import React from 'react'; //optional
+import React, { useState } from 'react';
+import axios from 'axios';
 import "./styles.scss";
 import Button from '../Button';
 const classNames = require('classnames');
 
 export default function Form(props) {
 
+  const [description, setDescription] = useState();
+  const [location, setLocation] = useState();
+
+  // handles the save button in the form and submits the input data to the database
   const handleClick = () => {
     console.log('save button was clicked.');
-    // return axios
-    //   .delete(`http://localhost:8080/api/tasks/${id}`)
+    return axios
+      .post(`http://localhost:8080/api/tasks`, {'description': description, 'location_id':  location})
   }
 
   return (
     <div className="form">
       <form>
-        <input className="description" type="text" placeholder="Add description" />
-        <input className="location" type="text" placeholder="Add location" />
+        <input 
+          className="description"
+          name="description" 
+          type="text" 
+          placeholder="Add description"
+          value={description}
+          onChange={(event) => {
+            setDescription(event.target.value);
+            console.log(event.target.value);
+          }}
+        />
+        <input 
+          className="location" 
+          name="location"
+          type="text" 
+          placeholder="Add location"
+          value={location}
+          onChange={(event) => {
+            setLocation(event.target.value);
+            console.log(event.target.value);
+          }}
+        />
       </form>
       <Button save onClick={() => handleClick()} />
     </div>
