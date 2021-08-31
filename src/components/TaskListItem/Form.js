a5d46b6bfb958f747c5bd0d915b78fad96520aca
Luana <luanateixeira148@gmail.com>
2021-08-31 12:18:31 -0700

    handle save button click

--- src/components/TaskListItem/Form.js
+++ src/components/TaskListItem/Form.js
@@ -5,13 +5,19 @@ const classNames = require('classnames');
 
 export default function Form(props) {
 
+  const handleClick = () => {
+    console.log('save button was clicked.');
+    // return axios
+    //   .delete(`http://localhost:8080/api/tasks/${id}`)
+  }
+
   return (
     <div className="form">
       <form>
         <input className="description" type="text" placeholder="Add description" />
         <input className="location" type="text" placeholder="Add location" />
       </form>
-      <Button save />
+      <Button save onClick={() => handleClick()} />
     </div>
   );
 
