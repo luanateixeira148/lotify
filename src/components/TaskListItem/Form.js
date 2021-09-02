cdfe06057c1e68a2bf142534a8ce1c55c756765f
Luana <luanateixeira148@gmail.com>
2021-09-02 14:03:43 -0700

    add react-select to FormOnEdit component

--- src/components/TaskListItem/Form.js
+++ src/components/TaskListItem/Form.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./styles.scss";
 import Button from '../Button';
-import Dropdown from './Dropdown';
 import Select from 'react-select';
 const classNames = require('classnames');
 
--- src/components/TaskListItem/FormOnEdit.js
+++ src/components/TaskListItem/FormOnEdit.js
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./styles.scss";
 import Button from '../Button';
+import Select from 'react-select';
 const classNames = require('classnames');
 
 export default function FormOnEdit(props) {
 
   const [description, setDescription] = useState(props.description);
   const [location, setLocation] = useState(props.location);
+  const [locations, setLocations] = useState([]);
 
+  /* Makes get request and sets locations to be shown on the dropdown menu */
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/locations")
+    .then(res => { 
+      setLocations(res.data);  
+    })
+  }, [])
+
+  /* Takes the locations from the axios request, and output it in the right format for the dropdown location */
+  const getLocations = function(locations) {
+    let output = [];
+    for (let location of locations) {
+      output.push({value: location.id, label: location.name + ', ' + location.address})
+    }
+    return output;
+  }
+
+  /* uses the getLocations function and passes the locations object to be used by the React-Select component */
+  const options = getLocations(locations)
+
+  /* Creates the put request to send new description and locationId to the db and updates the component */
   const editTask = () => {
     let id = props.id;
     axios
-      .put(`http://localhost:8080/api/tasks/edit/${id}?description=${description}&location_id=${location}`)
+      .put(`http://localhost:8080/api/tasks/edit/${id}?description=${description}&location_id=${location.value}`)
       .then((res) => {
         props.setEdit('show');
         props.setLoading(true);
@@ -31,10 +54,16 @@ export default function FormOnEdit(props) {
           value={description}
           onChange={(event) => {
             setDescription(event.target.value);
-            console.log(event.target.value);
           }}
         />
-        <input 
+        <div className="dropdown">
+            <Select
+              defaultValue={location}
+              onChange={setLocation}
+              options={options}
+            />
+          </div>
+        {/* <input 
           className="location" 
           name="location"
           type="text" 
@@ -43,7 +72,7 @@ export default function FormOnEdit(props) {
           onChange={(event) => {
             setLocation(event.target.value);
           }}
-        />
+        /> */}
       </form>
       <Button save onClick={() => editTask()} />
     </div>
