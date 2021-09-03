7b76b4e161a75f9ed0ccb71ae1b95a2ff53fc4e8
jan-lab <tnlatte@outlook.com>
2021-09-02 17:48:05 -0700

    implement bestroute function - 1st version

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,16 @@ import React from 'react';
 import "./Header.scss";
 
 export default function Header(props) {
+  // console.log('props.bestRoute',props.bestRoute); //array of objects
+  // console.log('props.bestRoute[0]',props.bestRoute[0]); //object with key called best_route_url
+
+  // without the conditional statement, props.bestRoute[0] becomes undefined
+  const best_route_url = props.bestRoute[0]? props.bestRoute[0].best_route_url : ''
 
   return (
     <div className="header">
       <h1>Lotify</h1>
-      <button>DIRECTIONS</button>
+      <a href={best_route_url}><button>Best Route</button></a>
     </div>
   );
 
