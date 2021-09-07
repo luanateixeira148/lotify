8ea39676bb6a9172974cf901b33a2319cc29774c
Luana <luanateixeira148@gmail.com>
2021-09-07 13:46:46 -0700

    bring axios request back to application page

--- src/components/ApplicationPage/index.js
+++ src/components/ApplicationPage/index.js
@@ -5,21 +5,42 @@ import Form from '../TaskListItem/Form';
 import Header from '../Header';
 import TaskList from '../TaskList';
 import Footer from '../Footer';
-import useApplicationData from '../../hooks/useApplicationData';
 
 export default function MainPage() {
 
-  const {
-    tasks, setTasks,
-    bestRoute
-  } = useApplicationData();
-
   const [formState, setFormState] = useState('hide');
+  const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [bestRoute, setBestRoute] = useState([]);
   const [fetchTasks, setFetchTasks] = useState(false);
 
-  return (
-    <div className="App">
+  // /* makes initial axios get request and sets initial tasks */
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/tasks")
+    .then(res => { 
+      setTasks(res.data);  
+      setLoading(false);
+      setFetchTasks(true);
+    })
+  }, [loading])
+
+  /* makes axios get request to set the right Google Maps URL for the best route button */
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/bestroute")
+    .then(res => { 
+      setBestRoute(res.data);
+    })
+  }, [fetchTasks])
+  
+  let displayObject;
+  
+  if(!fetchTasks) { //Initial Condition
+    displayObject = 
+    <div className="loading-screen">
+      <h1>Lotify</h1>
+    </div>
+  } else {
+    displayObject = <>
       <Header 
         bestRoute={bestRoute}
       />
@@ -37,71 +58,12 @@ export default function MainPage() {
         setFormState={setFormState}
         formState={formState}
       />
+    </>
+  }
+
+  return (
+    <div className="App">
+      {displayObject}
     </div>
   );
 }
-
-// export default function MainPage() {
-
-//   const {
-//     tasks, setTasks
-//   } = useApplicationData();
-
-//   const [formState, setFormState] = useState('hide');
-//   // const [tasks, setTasks] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [bestRoute, setBestRoute] = useState([]);
-//   const [fetchTasks, setFetchTasks] = useState(false);
-
-//   /* makes initial axios get request and sets initial tasks */
-//   // useEffect(() => {
-//   //   axios.get("http://localhost:8080/api/tasks")
-//   //   .then(res => { 
-//   //     setTasks(res.data);  
-//   //     setLoading(false);
-//   //     setFetchTasks(true);
-//   //   })
-//   // }, [loading])
-
-//   /* makes axios get request to set the right Google Maps URL for the best route button */
-//   useEffect(() => {
-//     axios.get("http://localhost:8080/api/bestroute")
-//     .then(res => { 
-//       setBestRoute(res.data);
-//     })
-//   }, [fetchTasks])
-//   let displayObject;
-  
-//   // if(!fetchTasks) { //Initial Condition
-//   //   displayObject = 
-//   //   <div className="loading-screen">
-//   //     <h1>Lotify</h1>
-//   //   </div>
-//   // } else {
-//     displayObject = <>
-//       <Header 
-//         bestRoute={bestRoute}
-//       />
-//       <Form 
-//         setFormState={setFormState}
-//         formState={formState}
-//         setLoading={setLoading}
-//       />
-//       <TaskList
-//         tasks={tasks}
-//         setTasks={setTasks}
-//         setLoading={setLoading}
-//       />
-//       <Footer
-//         setFormState={setFormState}
-//         formState={formState}
-//       />
-//     </>
-//   // }
-
-//   return (
-//     <div className="App">
-//       {displayObject}
-//     </div>
-//   );
-// }
